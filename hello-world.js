8ea5c5d2a5f38dc8ee741e55932e22108b996ccf
Kieron Hagg <loadedh@gmail.com>
2016-09-06 00:55:04 +0100

    Add files via upload

---
+++ hello-world.js
@@ -0,0 +1,13 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class HelloWorld {
+  hello() {
+
+    return "Hello, world!"
+  }
+}
+
+export default HelloWorld;
