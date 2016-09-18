4a0e9943ea8a3bdb5b93e448a0765d8b6dc4fbd2
Kieron Hagg <loadedh@gmail.com>
2016-09-18 21:50:22 +0100

    Create word-count.js

---
+++ word-count.js
@@ -0,0 +1,9 @@
+export default () => ({
+    count: (phrase) => phrase
+    .trim()
+    .split(/\s+/)
+    .reduce((counts, word) => {
+    counts[word]++ || (counts[word] = 1);
+return counts;
+    }, {})
+})
