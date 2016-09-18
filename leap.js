eb8e54b3aaba0e21b0126fb5ed9f829052aa8c6a
Kieron Hagg <loadedh@gmail.com>
2016-09-18 21:45:58 +0100

    Delete leap.js

--- leap.js
+++
@@ -1,8 +0,0 @@
-export default function isLeapYear (input) {
-    if (input % 4 === 0 && input % 100 != 0 || input % 400 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-
-}
