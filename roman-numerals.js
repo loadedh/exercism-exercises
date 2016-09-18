5b9ec2619684ee265e052cfd868fd9c735f7053c
Kieron Hagg <loadedh@gmail.com>
2016-09-18 22:10:20 +0100

    Create roman-numerals.js

---
+++ roman-numerals.js
@@ -0,0 +1,11 @@
+const ROMANS = {
+  1: ['','I','II','III','IV','V','VI','VII','VIII','IX'],
+  10: ['', 'X', 'XX', 'XXX', 'XL','L','LX','LXX','LXXX','XC'],
+  100: ['','C', 'CC','CCC','CD','D','DC','DCC','DCCC','CM'],
+  1000: ['','M','MM','MMM']
+};
+
+export default decimal => [...decimal.toString()]
+        .reduce((total, current, power) => {
+          if ()
+        }, 0)
