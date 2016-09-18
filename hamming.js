2cbfc5a8b0d96a7730dd734132cee28e339186b9
Kieron Hagg <loadedh@gmail.com>
2016-09-18 21:46:36 +0100

    Create hamming.js

---
+++ hamming.js
@@ -0,0 +1,16 @@
+export default class Hamming {
+    compute(a, b) {
+        if (a.length != b.length)
+            throw new Error('DNA strands must be of equal length.');
+
+        for (let i = 0; i < a.length; i++) {
+            let x = a[i], y = b[i];
+            if(x !== y)
+                distance++;
+        }
+
+        return distance;
+    }
+
+
+}
