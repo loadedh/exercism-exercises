baf681fa89b8050cc48857c5c7db6558809f0814
Kieron Hagg <loadedh@gmail.com>
2016-09-18 22:03:59 +0100

    Delete acronym.js

--- acronym.js
+++
@@ -1,6 +0,0 @@
-const BREAK_WORDS = /[A-Z]+[a-z]*|[a-z]+/g;
-export default {
-  parse: phrase => phrase.match(BREAK_WORDS)
-      .reduce((acronym, word) => acronym.concat(word[0]),'')
-      .toUpperCase()
-}
