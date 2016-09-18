5b8ce716f412dc062aec53fee77b14bef582e3bd
Kieron Hagg <loadedh@gmail.com>
2016-09-18 21:53:29 +0100

    Create anagram.js

---
+++ anagram.js
@@ -0,0 +1,16 @@
+let targetWord, sortedWord;
+
+export default word => {
+        targetWord = word;
+        sortedWord = sortChars(targetWord);
+        return {
+            matches: (possibles,...more) => {
+            Array.isArray(possibles) ? more = more.concat(possibles) : more.push(possibles);
+            return matches(more);
+        }
+    }
+}
+
+const sortChars = target => [...target.toLowerCase()].sort().join('');
+const matches = possibles => possibles.filter(match);
+const match = possible => targetWord !== possible.toLowerCase() && (sortedWord === sortChars(possible))
