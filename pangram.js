8605dbec95f1077359f1214a42a460278530df8f
Kieron Hagg <loadedh@gmail.com>
2016-09-18 21:50:57 +0100

    Create pangram.js

---
+++ pangram.js
@@ -0,0 +1,13 @@
+const notAlpha = /[^a-z]+/gi,
+    alphaLength = 26;
+
+export default class {
+    constructor (candidate){
+        const cleaned = candidate.replace(notAlpha,'').toLowerCase();
+        this.unique = new Set([...cleaned]);
+    }
+
+    isPangram (){
+        return this.unique.size === alphaLength;
+    }
+}
